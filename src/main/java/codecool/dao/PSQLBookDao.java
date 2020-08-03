0a3b8f7b3ac7a44b50e9a05172655d7f54d8cff4
Flloki5 <rozli@op.pl>
2020-08-03 16:27:07 +0200

    Added PSQLBookDao class

---
+++ src/main/java/codecool/dao/PSQLBookDao.java
@@ -0,0 +1,28 @@
+package codecool.dao;
+
+import codecool.model.Book;
+
+import java.util.List;
+
+public class PSQLBookDao implements BookDao {
+
+    @Override
+    public void add(Book book) {
+
+    }
+
+    @Override
+    public void delete(int bookId) {
+
+    }
+
+    @Override
+    public void edit(Book book, int book_id) {
+
+    }
+
+    @Override
+    public List<Book> getBooksList() {
+        return null;
+    }
+}
